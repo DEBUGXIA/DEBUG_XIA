7d98eb259ad41df1234fdf9f7a00e46643cf914d
SUBHADEEP BISWAS <147643078+subhadeep104647@users.noreply.github.com>
2026-03-19 17:43:39 +0530

    done

--- DEBUGXIA/src/components/Examole.jsx
+++ DEBUGXIA/src/components/Examole.jsx
@@ -26,11 +26,11 @@ const Examole = () => {
       }
   
     return (
-      <div className='p-20 bg-[url(public/Glass.svg)] bg-cover bg-black h-screen text-white lg:flex justify-between gap-10 '>
+      <div className='p-20 bg-[url(public/Glass.svg)] bg-cover bg-gray-950 h-screen text-white lg:flex justify-between gap-10 '>
   
         <form className=' flex-col lg:w-1/2 gap-5 justify-between border-white rounded-md py-2' onSubmit={(e) =>{SubmitHandler(e)}}>
 
-        <div className='font-bold text-2xl tracking-wide py-3'><Icon/>Your Coding Terminal</div>
+        <div className='font-bold text-2xl tracking-wide py-3'><Icon/>Your Coding <span className='bg-[url(public/bg.svg)] text-3xl bg-clip-text text-transparent'>Terminal</span></div>
   
           <div className='flex flex-col gap-5 justify-between border-white rounded-md'>
   
@@ -39,13 +39,13 @@ const Examole = () => {
           <textarea placeholder='Enter Your Code . . . ' name="" id=""className='px-5 py-2 font-semibold text-lg tracking-wide border-white rounded-md border-1 h-60'value={details} onChange={(e) => {setDetails(e.target.value)}}></textarea>
   
           <div className='flex items-center justify-center'>
-            <button className='bg-blue-300 text-white px-4 py-2 font-semibold text-lg tracking-wide border-white border-2 rounded-2xl w-50 text-center cursor-pointer active::sc9'>Run Your Code</button>
+            <button className=' text-white px-4 py-2 font-semibold text-lg tracking-wide rounded-2xl w-50 text-center cursor-pointer active::sc9 bg-[url(public/btn.svg)] bg-cover '>Run Your Code</button>
           </div>
           </div>
         </form>
         <div className=' flex flex-wap flex-col justify-start p-2 w-1/2 h-150 gap-2'>
   
-        <div className='font-bold text-2xl tracking-wide'><Icon/>Your Coding Teacher</div>
+        <div className='font-bold text-2xl tracking-wide'><Icon/>Your Coding <span className='bg-[url(public/bg.svg)] text-3xl bg-clip-text text-transparent'>Teacher</span></div>
   
         {file.map(function(elem,idx){
             return <div key={idx} className='font-bold text-xl tracking-wide'>{elem.title}</div>
--- DEBUGXIA/src/components/header/Sing_in_up.jsx
+++ DEBUGXIA/src/components/header/Sing_in_up.jsx
@@ -5,7 +5,7 @@ const Sing_in_up = () => {
 
       <div className='list-none flex items-center flex-row justify-end gap-10 absolute right-10 '>
         <li><a className='text-base font-semibold tracking-wide'>SingIn</a></li>
-        <button className=' bg-blue-300 text-white py-1.5 px-4 text-lg font-semibold rounded-2xl cursor-pointer active::sc9 tracking-wide border-white border-2'>Get Started</button>
+        <button className=' text-white py-1.5 px-4 text-lg font-semibold rounded-2xl cursor-pointer active::sc9 tracking-wide bg-[url(public/Btn.svg)] bg-cover'>Get Started</button>
     </div>
   )
 }
