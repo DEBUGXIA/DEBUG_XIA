6c1dff730775e4c2b66c6cbe431b91cb3c72c710
SUBHADEEP BISWAS <147643078+subhadeep104647@users.noreply.github.com>
2026-03-25 12:51:34 +0530

    profile working . . .

--- DEBUGXIA/src/components/header/Profileh.jsx
+++ DEBUGXIA/src/components/header/Profileh.jsx
@@ -5,7 +5,7 @@ import Profile from '../../pages2.o/Profile'
 const Profileh = () => {
   return (
   <div className='list-none flex items-center flex-row justify-end gap-10 absolute right-10 text-base font-semibold tracking-wide'>
-        <button className='-mr-30 h-50 w-50'><Link to='/Profile'><img src="/public/User.jpg" alt=""  className='w-13 h-13 border-2 border-gray-500 rounded-full'/></Link></button>
+        <button className='-mr-30 h-50 w-50'><Link to='/Profile'><img src="/public/User.jpeg" alt=""  className='w-8 h-8 border-1 border-gray-500 rounded-full'/></Link></button>
     </div>
   )
 }
--- DEBUGXIA/src/pages2.o/Profile.jsx
+++ DEBUGXIA/src/pages2.o/Profile.jsx
@@ -1,35 +1,50 @@
 import React from 'react'
+import {NotebookPen} from 'lucide-react'
+import {UserRoundPen} from 'lucide-react'
 
 const Profile = () => {
   return (
     <div className='flex flex-col items-center justify-between mt-10 px-20'>
-      <div className=' flex flex-row items-center justify-between bg-amber-300 w-full gap-5'>
-        <div className=' flex flex-col items-center justify-between gap-2 w-1/3 bg-green-300'>
+      <div className=' flex flex-row items-center justify-between bg-amber- w-full gap-5'>
+        <div className=' flex flex-col items-center justify-between gap-2 w-1/4 bg-green-'>
           <div>
-            <img src="/public/User.jpg" alt="" className=' w-70 h-70 border-2 border-gray-500 rounded-full'/>
+            <img src="/public/User.jpeg" alt="" className=' w-70 h-70 border-2 border-gray-500 rounded-full'/>
           </div>
           <div className=' bg-gray-900 border-2 border-gray-500 rounded-xl text-gray-400 py-1.5 px-4 tracking-wide'>
             <h1>Edit Profile Picture</h1>
           </div>
+
+          <div className=' flex flex-row items-center justify-between gap-1.5'>
+            <div><UserRoundPen color="#ffffff" strokeWidth={1.25} /></div>
+            <div>
+              <input type="text" placeholder='Enter UserName' className='px-4 py-1.5 text-gray-400 bg-gray-900 font-normal text-lg tracking-wide border-gray-500 border-1 rounded-xl w-70' />
+            </div>
+          </div>
+
+          <div className=' flex flex-row items-center justify-between gap-1.5'>
+            <div><NotebookPen color="#ffffff" strokeWidth={1.25} /></div>
+            <div>
+              <textarea placeholder='Enter Your Boi . . . ' name="" id=""className='bg-gray-900 px-5 py-2 font-normal text-lg tracking-wide border-gray-500 rounded-xl border-1 h-20 w-70 text-gray-400'></textarea>
+            </div>
+          </div>
+
+          <div className=' flex flex-row items-center justify-between gap-1.5 bg-gray-900 px-5 py-2 font-semibold text-lg tracking-wide border-gray-500 rounded-xl border-1  w-80 -'>
+            <label for="cars" className=' text-gray-400'>Pronouns :</label>
+
+              <select name="cars" id="cars"  className='bg-gray-900 text-gray-400 w-40 font-normal text-lg'>
+                <option >Don't Specify</option>
+                <option >They/Them</option>
+                <option >She/Her</option>
+                <option >He/Him</option>
+                <option >Custom</option>
+              </select>
+          </div>
+
         </div>
 
-        <div className='w-2/3 bg-gray-900 flex flex-col items-center justify-between gap-2 border-2 border-gray-500 h-80 rounded-2xl'>
-          <div className=' flex flex-row items-center justify-baseline gap-2 h-1/2 w-full'>
-            <div className=' w-1/2 -mt-20 ml-3'>
-              <input type="text" placeholder='Enter UserName' className='px-5 py-2 text-white font-semibold text-lg tracking-wide border-gray-600 border-1 rounded-xl w-full'/>
-            </div>
-            <div className='  w-1/2 -mt-20 mr-3'>
-              <input type="text" placeholder='Enter UserName' className='px-5 py-2 text-white font-semibold text-lg tracking-wide border-gray-600 border-1 rounded-xl w-full'/>
-            </div>
-          </div>
-          <div className=' flex flex-row items-center justify-baseline gap-2 h-1/2 w-full'>
-            <div className='  w-1/2 -mt-70 ml-3'>
-              <input type="text" placeholder='Enter UserName' className='px-5 py-2 text-white font-semibold text-lg tracking-wide border-gray-600 border-1 rounded-xl w-full'/>
-            </div>
-            <div className='  w-1/2 -mt-70 mr-3'>
-              <input type="text" placeholder='Enter UserName' className='px-5 py-2 text-white font-semibold text-lg tracking-wide border-gray-600 border-1 rounded-xl w-full'/>
-            </div>
-          </div>
+        <div className='w-3/4 bg-gray-900 flex flex-col items-center justify-between gap-2 border-2 border-gray-500 h-80 rounded-2xl -mt-50'>
+          
+          
         </div>
 
       </div>
