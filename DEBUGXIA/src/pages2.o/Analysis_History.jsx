dd52fb25063d1f439906681f8aaf7b278cd6ef26
SUBHADEEP BISWAS <147643078+subhadeep104647@users.noreply.github.com>
2026-03-28 17:06:21 +0530

    done

--- DEBUGXIA/src/pages2.o/Analysis_History.jsx
+++ DEBUGXIA/src/pages2.o/Analysis_History.jsx
@@ -4,6 +4,7 @@ import {ChartNoAxesCombined} from 'lucide-react'
 import { FileText } from 'lucide-react'
 import { CalendarDays } from 'lucide-react'
 import { CodeXml } from 'lucide-react'
+import { BadgeAlert } from 'lucide-react'
 
 
 const Analysis_History = () => {
@@ -15,8 +16,56 @@ const Analysis_History = () => {
     { Title: "Time Saved", Num: "450m", Logo: <CalendarDays color="#69f781" strokeWidth={1.25} /> },
   ];
 
+  const Data = [
+  {
+    file: "App.tsx",
+    type: "TypeScript",
+    time: "2 hours ago",
+    error: 35,
+    quality: 72,
+    success: 58,
+    issues: 3
+  },
+  {
+    file: "utils.ts",
+    type: "TypeScript",
+    time: "5 hours ago",
+    error: 15,
+    quality: 85,
+    success: 72,
+    issues: 1
+  },
+  {
+    file: "handler.py",
+    type: "Python",
+    time: "1 day ago",
+    error: 52,
+    quality: 61,
+    success: 45,
+    issues: 5
+  },
+  {
+    file: "Service.js",
+    type: "JavaScript",
+    time: "2 day ago",
+    error: 28,
+    quality: 78,
+    success: 65,
+    issues: 2
+  },
+  {
+    file: "Index.html",
+    type: "HTML",
+    time: "3 day ago",
+    error: 10,
+    quality: 90,
+    success: 80,
+    issues: 0
+  }
+];
+
   return (
-    <div className='flex flex-col justify-between ml-10 mt-10 gap-8 w-full px-10'>
+    <div className='flex flex-col justify-between ml-10 mt-10 gap-10 w-screen px-10'>
 
       <div className='flex flex-col items-start gap-2 mt-3 ml-5'>
         <h1 className='bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent font-bold text-4xl'>
@@ -33,7 +82,7 @@ const Analysis_History = () => {
           <div
             key={idx}
             className='flex flex-row items-center gap-8 bg-gray-900 border border-gray-400 rounded-xl px-4 p-4 
-            hover:border-blue-500 hover:shadow-blue-500/20 hover:shadow-lg 
+            hover:border-blue-500 hover:shadow-blue-500/20  
             transition-all duration-300 w-1/4'
           >
             <div>{item.Logo}</div>
@@ -47,48 +96,66 @@ const Analysis_History = () => {
 
       </div>
 
-      <div className='flex flex-col items-start gap-8 bg-gray-900 border border-gray-400 rounded-xl px-4 p-4 
-            hover:border-blue-500 hover:shadow-blue-500/20 hover:shadow-lg 
-            transition-all duration-300 h-1/4 mr-20'>
 
-        <div className=' flex flex-col items-center justify-between gap-5'>
-          <h1 className=' font-bold text-xl tracking-wide'>Recent Analyses</h1>
-        </div>
+      <div className='flex flex-col items-start gap-8 bg-gray-900 border border-gray-400 rounded-xl px-4 p-4 
+             hover:shadow-blue-500/20  
+            transition-all duration-300  mr-20 px-10 py-5'>
+
+              <div className=' flex flex-col items-center justify-between gap-5'>
+                <h1 className=' font-bold text-xl tracking-wide'>Recent Analyses</h1>
+              </div>
+
+        {Data.map((item,idx)=>(
+
+          <div key={idx} >
+
+        
 
         <div className='flex flex-col items-start gap-3 bg-gray-900 border border-gray-400 rounded-xl px-4 p-4 
-            hover:border-blue-500 hover:shadow-blue-500/20 hover:shadow-lg 
-            transition-all duration-300 w-full '>
+            hover:border-blue-500 transition-all duration-300 w-320'>
 
           <div className=' flex flex-row items-center justify-between gap-3'>
             <div><CodeXml color="#607ceb" strokeWidth={1} /></div>
-            <div><h3 className=' font-bold text-lg tracking-wide text-gray-300'>App.tsx</h3></div>
-            <div><p className='font-normal text-sm tracking-wide text-gray-400 bg-gray-600 border-0 rounded-lg px-2.5 py-0.5'>TypeScript</p></div>
+            <div><h3 className=' font-bold text-lg tracking-wide text-gray-300'>{item.file}</h3></div>
+            <div><p className='font-normal text-sm tracking-wide text-gray-400 bg-gray-800 border-0 rounded-lg px-2.5 py-1'>{item.type}</p></div>
           </div>
 
           <div className=' flex flex-row items-start justify-between gap-1'>
             <div><CalendarDays color="#8a8a8a" strokeWidth={0.75} /></div>
-            <div><p className=' font-normal text-sm tracking-wide text-gray-600'>2 hours ago</p></div>
+            <div><p className=' font-normal text-sm tracking-wide text-gray-600'>{item.time}</p></div>
           </div>
 
           <div className=' flex flex-row items-center justify-between gap-2 w-full'>
-            <div className=' flex flex-col items-center justify-between gap-0.5 bg-red-900 border-0 rounded-xl w-1/3'>
+            <div className=' flex flex-col items-center justify-between gap-0.5 bg-red-950 border-0 rounded-xl w-1/3'>
               <div><h1 className=' font-medium text-lg text-red-300'>Error</h1></div>
-              <div><h1 className=' font-bold text-xl text-red-300'>72%</h1></div>
+              <div><h1 className=' font-bold text-xl text-red-300'>{item.error}%</h1></div>
             </div>
 
-            <div className=' flex flex-col items-center justify-between gap-1 bg-red-900 border-0 rounded-xl w-1/3'>
-              <div><h1 className=' font-medium text-lg text-red-300'>Error</h1></div>
-              <div><h1 className=' font-bold text-xl text-red-300'>72%</h1></div>
+            <div className=' flex flex-col items-center justify-between gap-1 bg-blue-950 border-0 rounded-xl w-1/3'>
+              <div><h1 className=' font-medium text-lg text-blue-300'>Quality</h1></div>
+              <div><h1 className=' font-bold text-xl text-blue-300'>{item.quality}%</h1></div>
             </div>
 
-            <div className=' flex flex-col items-center justify-between gap-1 bg-green-900 border-0 rounded-xl w-1/3'>
-              <div><h1 className=' font-medium text-lg text-green-300'>Error</h1></div>
-              <div><h1 className=' font-bold text-xl text-green-300'>72%</h1></div>
+            <div className=' flex flex-col items-center justify-between gap-1 bg-green-950 border-0 rounded-xl w-1/3'>
+              <div><h1 className=' font-medium text-lg text-green-300'>Optimization</h1></div>
+              <div><h1 className=' font-bold text-xl text-green-300'>{item.success}%</h1></div>
             </div>
           </div>
+
+          <div className=' flex flex-row items-center justify-between gap-1'>
+            <div><BadgeAlert color="#c05fec" strokeWidth={0.75} /></div>
+            <div><h1 className='font-normal text-sm tracking-wide text-purple-400'>{item.issues} issues found</h1></div>
+          </div>
+
         </div>
 
       </div>
+
+        ))}
+      </div>
+
+
+
 
     </div>
   )
--- DEBUGXIA/src/pages2.o/Error_History.jsx
+++ DEBUGXIA/src/pages2.o/Error_History.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Search } from 'lucide-react'
 
 const History = () => {
   return (
-    <div className=' flex flex-col justify-between ml-20 mt-10'>
+    <div className=' flex flex-col justify-between ml-20 mt-10 gap-8 w-screen'>
 
         <div className=' flex flex-col items-start justify-between gap-2 mt-3 ml-5'>
             <div className=''>
@@ -14,7 +15,26 @@ const History = () => {
         </div>
 
         <div>
-          <div></div>
+
+          <div className=' flex flex-row items-start justify-between gap-5 w-full'>
+
+            <div className=' w-2/3'>
+              <search className=' flex flex-row items-start gap-1 w-full  border-1 border-gray-400 rounded-xl bg-gray-900 px-5 py-2 '>
+                <div><Search color="#c0bec1" strokeWidth={0.75} /></div>
+                <div>
+                  <form>
+                  <input name="fsrch" id="fsrch" placeholder="Search Error . . ."/>
+                </form>
+                </div>
+              </search>
+            </div>
+
+            <div className=' flex flex-row items-start justify-between gap-2 w-1/3'>
+              
+            </div>
+
+          </div>
+
         </div>
 
     </div>
