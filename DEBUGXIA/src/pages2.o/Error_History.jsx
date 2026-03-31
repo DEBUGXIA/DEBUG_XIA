430f8d18be492b7eb4c53887d5b1fd6a7b0a0dd3
SUBHADEEP BISWAS <147643078+subhadeep104647@users.noreply.github.com>
2026-03-31 12:25:12 +0530

    working . . . . . . . .

--- DEBUGXIA/src/pages2.o/Error_History.jsx
+++ DEBUGXIA/src/pages2.o/Error_History.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Search } from 'lucide-react'
+import { Search, BadgeAlert } from 'lucide-react'
 
 const History = () => {
 
@@ -20,9 +20,9 @@ const History = () => {
             </div>
         </div>
 
-        <div>
+        <div className=' flex flex-col items-center justify-between gap-3 mr-10'>
 
-          <div className=' flex flex-row items-start justify-between gap-5 w-full'>
+          <div className=' flex flex-row items-center justify-between gap-5 w-full mr-10'>
 
             <div className=' w-2/3'>
               <search className=' flex flex-row items-start gap-1 w-full  border-1 border-gray-400 rounded-xl bg-gray-900 px-5 py-2 '>
@@ -35,40 +35,62 @@ const History = () => {
               </search>
             </div>
 
-            <div className="flex flex-row items-start justify-between gap-2 w-1/3 bg-blue-300 mr-20">
+            <div className="flex flex-row items-start justify-between gap-2 w-1/3 mr-15">
   
-              <div className="">
-      
-      
-      <button
-        onClick={() => setOpen(!open)}
-        className="bg-gray-900 text-white border border-gray-700 px-4 py-2 rounded-lg flex items-center gap-2 hover:border-blue-500 transition"
-      >
-        {selected}
-        <span className="text-gray-400">▼</span>
-      </button>
+                <div className=" flex flex-row w-1/2">
+                  <button onClick={() => setOpen(!open)} className="bg-gray-900 text-white border border-gray-700 px-4 py-2 rounded-lg flex items-center gap-2 hover:border-blue-500 transition w-50">
+                    {selected}<span className="text-gray-400">▼</span>
+                  </button>
+                  {open && (
+                  <div className="absolute mt-2 w-50 bg-gray-900 rounded-md shadow-lg z-50">
+                    {options.map((item, index) => (
+                  <div
+                    key={index}
+                    onClick={() => {
+                    setSelected(item);
+                    setOpen(false);
+                    }}
+                    className="px-4 py-2 text-gray-200 hover:bg-gray-900 cursor-pointer">
+                    {item}
+                  </div>
+                   ))}
+                  </div>
+                   )}
+                </div>
 
-      
-      {open && (
-        <div className="absolute mt-2 w-full bg-gray-200 rounded-md shadow-lg z-50">
-          {options.map((item, index) => (
-            <div
-              key={index}
-              onClick={() => {
-                setSelected(item);
-                setOpen(false);
-              }}
-              className="px-4 py-2 text-gray-800 hover:bg-gray-300 cursor-pointer"
-            >
-              {item}
+                <div className=" flex flex-row w-1/2">
+                  <button onClick={() => setOpen(!open)} className="bg-gray-900 text-white border border-gray-700 px-4 py-2 rounded-lg flex items-center gap-2 hover:border-blue-500 transition w-50">
+                    {selected}<span className="text-gray-400">▼</span>
+                  </button>
+                  {open && (
+                  <div className="absolute mt-2 w-50 bg-gray-900 rounded-md shadow-lg z-50">
+                    {options.map((item, index) => (
+                  <div
+                    key={index}
+                    onClick={() => {
+                    setSelected(item);
+                    setOpen(false);
+                    }}
+                    className="px-4 py-2 text-gray-200 hover:bg-gray-900 cursor-pointer">
+                    {item}
+                  </div>
+                   ))}
+                  </div>
+                   )}
+                </div>
+
             </div>
-          ))}
-        </div>
-      )}
-    </div>
 
+
+          </div>
+
+          <div className=' flex flex-col items-center justify-between border-0 h-200'>
+            <div className=' flex flex-col items-center justify-center'>
+              <div className=' flex flex-col items-center justify-between gap-2 mt-20'>
+              <div><BadgeAlert color="#878787" strokeWidth={1.5} /></div>
+              <div><h1 className=' text-xl text-gray-500 font-medium'>No errors logged yet. Connect the VS Code extension to start tracking.</h1></div>
             </div>
-
+            </div>
           </div>
 
         </div>
